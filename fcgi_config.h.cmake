71bd780afef05d69ec3af6d8765913786431cd96
Peter Drahoš <drahosp@gmail.com>
2010-10-01 03:12:13 +0200

    Import to git

---
+++ fcgi_config.h.cmake
@@ -0,0 +1,111 @@
+/* fcgi_config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if you have the <arpa/inet.h> header file. */
+#cmakedefine HAVE_ARPA_INET_H ${HAVE_ARPA_INET_H}
+
+/* Define if you have the <dlfcn.h> header file. */
+#cmakedefine HAVE_DLFCN_H ${HAVE_DLFCN_H}
+
+/* Define if there's a fileno() prototype in stdio.h */
+#cmakedefine HAVE_FILENO_PROTO ${HAVE_FILENO_PROTO}
+
+/* Define if the fpos_t typedef is in stdio.h */
+#cmakedefine HAVE_FPOS ${HAVE_FPOS}
+
+/* Define if you have the <inttypes.h> header file. */
+#cmakedefine HAVE_INTTYPES_H ${HAVE_INTTYPES_H}
+
+/* Define if cin/cout/cerr has a streambuf assignment operator */
+#cmakedefine HAVE_IOSTREAM_WITHASSIGN_STREAMBUF ${HAVE_IOSTREAM_WITHASSIGN_STREAMBUF}
+
+/* Define if you have the `nsl' library (-lnsl). */
+#cmakedefine HAVE_LIBNSL ${HAVE_LIBNSL}
+
+/* Define if you have the `socket' library (-lsocket). */
+#cmakedefine HAVE_LIBSOCKET ${HAVE_LIBSOCKET}
+
+/* Define if you have the <limits.h> header file. */
+#cmakedefine HAVE_LIMITS_H ${HAVE_LIMITS_H}
+
+/* Define if you have the <memory.h> header file. */
+#cmakedefine HAVE_MEMORY_H ${HAVE_MEMORY_H}
+
+/* Define if you have the <netdb.h> header file. */
+#cmakedefine HAVE_NETDB_H ${HAVE_NETDB_H}
+
+/* Define if you have the <netinet/in.h> header file. */
+#cmakedefine HAVE_NETINET_IN_H ${HAVE_NETINET_IN_H}
+
+/* Define if you have POSIX threads libraries and header files. */
+#cmakedefine HAVE_PTHREAD ${HAVE_PTHREAD}
+
+/* Define if sockaddr_un in sys/un.h contains a sun_len component */
+#cmakedefine HAVE_SOCKADDR_UN_SUN_LEN ${HAVE_SOCKADDR_UN_SUN_LEN}
+
+/* Define if the socklen_t typedef is in sys/socket.h */
+#cmakedefine HAVE_SOCKLEN ${HAVE_SOCKLEN}
+
+/* Define if you have the <stdint.h> header file. */
+#cmakedefine HAVE_STDINT_H ${HAVE_STDINT_H}
+
+/* Define if you have the <stdlib.h> header file. */
+#cmakedefine HAVE_STDINT_H ${HAVE_STDINT_H}
+
+/* Define if char_type is defined in the context of streambuf */
+#cmakedefine HAVE_STREAMBUF_CHAR_TYPE ${HAVE_STREAMBUF_CHAR_TYPE}
+
+/* Define if you have the `strerror' function. */
+#cmakedefine HAVE_STRERROR ${HAVE_STRERROR}
+
+/* Define if you have the <strings.h> header file. */
+#cmakedefine HAVE_STRINGS_H ${HAVE_STRINGS_H}
+
+/* Define if you have the <string.h> header file. */
+#cmakedefine HAVE_STRING_H ${HAVE_STRING_H}
+
+/* Define if you have the <sys/param.h> header file. */
+#cmakedefine HAVE_SYS_PARAM_H ${HAVE_SYS_PARAM_H}
+
+/* Define if you have the <sys/socket.h> header file. */
+#cmakedefine HAVE_SYS_SOCKET_H ${HAVE_SYS_SOCKET_H}
+
+/* Define if you have the <sys/stat.h> header file. */
+#cmakedefine HAVE_SYS_STAT_H ${HAVE_SYS_STAT_H}
+
+/* Define if you have the <sys/time.h> header file. */
+#cmakedefine HAVE_SYS_TIME_H ${HAVE_SYS_TIME_H}
+
+/* Define if you have the <sys/types.h> header file. */
+#cmakedefine HAVE_SYS_TYPES_H ${HAVE_SYS_TYPES_H}
+
+/* Define if you have the <unistd.h> header file. */
+#cmakedefine HAVE_UNISTD_H ${HAVE_UNISTD_H}
+
+/* Define if va_arg(arg, long double) crashes the compiler */
+#cmakedefine HAVE_VA_ARG_LONG_DOUBLE_BUG ${HAVE_VA_ARG_LONG_DOUBLE_BUG}
+
+/* Name of package */
+#cmakedefine PACKAGE ${PACKAGE}
+
+/* Define to the necessary symbol if this constant uses a non-standard name on
+   your system. */
+#cmakedefine PTHREAD_CREATE_JOINABLE ${PTHREAD_CREATE_JOINABLE}
+
+/* Define if you have the ANSI C header files. */
+#cmakedefine STDC_HEADERS ${STDC_HEADERS}
+
+/* Define if cross-process locking is required by accept() */
+#cmakedefine USE_LOCKING ${USE_LOCKING}
+
+/* Version number of package */
+#cmakedefine VERSION ${VERSION}
+
+/* Define to empty if `const' does not conform to ANSI C. */
+#cmakedefine const ${CONST}
+
+/* Define as `__inline' if that's what the C compiler calls it, or to nothing
+   if it is not supported. */
+#cmakedefine inline ${INLINE}
+
+/* Define to `int' if <sys/types.h> does not define. */
+#cmakedefine ssize_t ${SSIZE_T}
